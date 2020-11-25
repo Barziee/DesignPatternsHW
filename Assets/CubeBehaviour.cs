1eddb5f32b77d3f95056b82a071980b74ca103c4
Bar Ronen <barzie02@gmail.com>
2020-11-26 00:02:18 +0200

    Wack-A-Mole like game. Used the singleton pattern for the score count

---
+++ Assets/CubeBehaviour.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    public GameObject cube;
+    public Rigidbody rb;
+    public BoxCollider bc;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                bc = hit.collider as BoxCollider;
+                if (bc != null)
+                {
+                    Debug.Log("HIT");
+                    CubeSpawner();
+                    GameManager.Instance.IncreaseScore();
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+    }
+
+    private void CubeSpawner()
+    {
+        bool cubeSpawned = false;
+        while (!cubeSpawned)
+        {
+            Vector3 cubePos = new Vector3(Random.Range(-4.5f, 4.5f), 1f, Random.Range(-4.5f, 4.5f));
+            if ((cubePos - transform.position).magnitude < 3)
+            {
+                continue;
+            }
+            else
+            {
+                Instantiate(cube, cubePos, Quaternion.identity);
+                cubeSpawned = true;
+            }
+        }
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [HideInInspector]
+    public TMP_Text scoreText;
+    public TMP_Text timer;
+    public BoxCollider bc;
+    public int scoreNumber = 0;
+    float _timer;
+
+    private void Awake()
+    {
+        Instance = this;
+        scoreText = GetComponent<TMP_Text>();
+
+        Invoke("Test", 5f);
+        _timer = 0;
+    }
+
+    public void IncreaseScore()
+    {
+        scoreNumber++;
+        scoreText.text = scoreNumber.ToString();
+    }
+
+    public void Test()
+    {
+        Debug.Log("TESTING");
+    }
+
+    
+    public IEnumerator Timer()
+    {
+        //Start game ? init/reset
+        while (_timer <= 30)
+        {
+            if(_timer < 10)
+            {
+                timer.text = "0" + _timer.ToString();
+            }
+            else
+            {
+                timer.text = _timer.ToString();
+            }
+            _timer++;
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        Debug.Log("DONE");
+        //Stop game
+    }
+
+    public void CallTimer()
+    {
+        StartCoroutine("Timer");
+    }
+}
+
